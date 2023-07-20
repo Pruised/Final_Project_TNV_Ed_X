c6103746b1542ba9b92e82787727329234859fec
Pruised <ink@live.it>
2023-07-20 15:46:43 +0200

    20.07.23  15.45

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RegisterComponent } from "./@shared/components/register/register.compon
 import { WelcomeComponent } from "./components/welcome/welcome.component";
 import { ProfileComponent } from "./components/profile/profile.component";
 import { RankingsComponent } from "./components/rankings/rankings.component";
+import { GiocaComponent } from './components/gioca/gioca.component';
 
 const routes: Routes = [
   {
@@ -17,6 +18,7 @@ const routes: Routes = [
       { path: "welcome", component: WelcomeComponent },
       { path: "profile", component: ProfileComponent },
       { path: "rankings", component: RankingsComponent },
+      { path: "gioca", component: GiocaComponent },
       { path: "", redirectTo: "welcome", pathMatch: 'full' },
     ],
   },
