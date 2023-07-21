2d9b2b7e9539c76a6bfd0f24222adf12dca3042d
SimoneZanata.88@gmail.com <you@example.com>
2023-07-21 18:31:00 +0200

    added Spring service

---
+++ SpringBoot/raccoltaUtenti/src/main/java/com/thenetvalue/raccoltaUtenti/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.thenetvalue.raccoltaUtenti.controller;
+
+import com.thenetvalue.raccoltaUtenti.model.User;
+import com.thenetvalue.raccoltaUtenti.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:4200/")
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User addUser(@RequestBody User user) {
+            return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public User loginUser(@RequestBody User user) {
+        return  userService.logIn(user.getUsername(), user.getPassword());
+    }
+
+    @GetMapping("users/{id}")
+    public Optional<User> getUserById(@PathVariable("id") int id) {
+        return userService.getUser(id);
+    }
+
+    @GetMapping("users/search/username/{partialUsername}")
+    public List<User> searchUserByUsername(@PathVariable("partialUsername") String partialUsername) {
+        return userService.searchUserByUsername(partialUsername);
+    }
+
+    @GetMapping("users/search/username/{partialUsername}/mail/{partialMail}")
+    public List<User> searchUserByUsername(@PathVariable("partialUsername") String partialUsername,
+                                           @PathVariable("partialMail") String partialMail) {
+        return userService.searchUserByUsernameAndEmail(partialUsername, partialMail);
+    }
+
+    @GetMapping("users/all")
+    public Iterable<User> allUsers() {
+        return userService.allUsers();
+    }
+
+    @PutMapping("users/{id}")
+    public String updateUser(@PathVariable("id") int id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("users/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        return userService.deleteUser(id);
+    }
+
+}
